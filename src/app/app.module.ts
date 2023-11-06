4989377803bcac82be2aa66a7708cbcf5b1face9
pausm99 <pausm99@gmail.com>
2023-11-06 09:39:12 +0100

    Exercici 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { EscenaComponent } from './components/home/escena/escena.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { EscenaComponent } from './components/home/escena/escena.component';
     EscenaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/escena/escena.component.ts
+++ src/app/components/home/escena/escena.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { iStep } from 'src/app/interfaces/istep.interface';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.css']
+  styleUrls: ['./escena.component.css'],
+  animations: [
+    trigger('changeElement', [
+      state('in', style({ opacity: 1 , transform: 'translateX(0%)'})),
+      state('out', style({ opacity: 0 , transform: 'translateX(100%)'})),
+      transition('in => out', animate('0.5s')),
+      transition('out => in', animate('0.5s')),
+    ]),
+  ],
 })
 export class EscenaComponent {
 
@@ -12,17 +21,40 @@ export class EscenaComponent {
   public frasesList: iStep[] = [];
 
   public currentStep: number = 0;
-
-  public stepBack(): void {
-    if (this.currentStep !== 0) --this.currentStep;
-  }
+  public nextStep: number = 0;
+  public changeState: 'in' | 'out' = 'in';
 
   public stepForward(): void {
-    if (this.currentStep !== this.frasesList.length-1) ++this.currentStep;
+    if (this.currentStep !== this.frasesList.length - 1) {
+      this.changeState = 'out';
+      this.nextStep = this.currentStep + 1;
+
+      setTimeout(() => {
+        this.currentStep = this.nextStep;
+        this.changeState = 'in';
+      }, 500); // Ajusta el tiempo según la duración de la animación
+    }
+  }
+
+  public stepBack(): void {
+    if (this.currentStep !== 0) {
+      this.changeState = 'out';
+      this.nextStep = this.currentStep - 1;
+
+      setTimeout(() => {
+        this.currentStep = this.nextStep;
+        this.changeState = 'in';
+      }, 500); // Ajusta el tiempo según la duración de la animación
+    }
   }
 
   public moveToDot(i: number): void {
-    this.currentStep = i;
-  }
+    this.changeState = 'out';
+    this.nextStep = i;
 
+    setTimeout(() => {
+      this.currentStep = this.nextStep;
+      this.changeState = 'in';
+    }, 500);
+  }
 }
